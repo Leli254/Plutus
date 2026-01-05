88067d77ce974f533e8ed53ce9cc3a29aacf22d2
Mike <lelimike@gmail.com>
2026-01-05 20:19:01 +0300

    documentation

---
+++ __init__.py
@@ -0,0 +1,12 @@
+
+"""
+Workers package for asynchronous background processing.
+
+Contains:
+- Consumer worker(s) for processing RawEvents
+- Future Celery / task workers
+"""
+
+__all__ = [
+    "consumer",
+]
